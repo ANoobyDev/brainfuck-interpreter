cdf7f5b093297f76c40a5b3f352d498a931a46f7
ANoobyDev <79359201+ANoobyDev@users.noreply.github.com>
2022-02-07 09:20:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LENGTH 9999
-
-/*
- * How does brainfuck work?
- * +   -> changes the value of the cell by 1
- * -   -> changes the value of the cell by -1
- * >   -> changes the cell by 1
- * <   -> changes the cell by -1
- * .   -> prints the int value of the cell
- * :   -> prints the ascii value of the cell
- * ,   -> accepts int input and adds it to the cell
- * ;   -> gets the ascii input and adds its number to the cell
- * []  -> loops if the value of the cell it ends on isn't 0
- */
-
 int main(int argc, char *argv[]){
     
     // Check if all the needed arguments are provided
